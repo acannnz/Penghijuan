b18e04f09682b43b2e96632dbabdf2d3afe314ec
Candra Pratama <putu.candra689@gmail.com>
2024-12-19 19:25:40 +0800

    update minor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       
